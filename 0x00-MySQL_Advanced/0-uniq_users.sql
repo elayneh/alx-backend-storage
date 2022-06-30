c783b5cce0cc8cc534b8d70dbcb82531acc3fa24
elayneh <betenatefera@gmail.com>
2022-06-30 13:52:31 +0300

    files moved

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,9 @@
-CREATE TABLE if not exists users(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+-- Create table with
+-- If table exists the query should not fail
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
+    name VARCHAR(255),
+    PRIMARY KEY(id)
 );
