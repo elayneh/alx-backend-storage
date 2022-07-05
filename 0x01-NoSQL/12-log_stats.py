948fe851bb72d0b21058632bf0a81bd940af9605
elayneh <betenatefera@gmail.com>
2022-07-06 00:00:03 +0300

    task added

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -20,15 +20,14 @@ if __name__ == "__main__":
     print(f"{check_get} status check")
 
     print("IPs:")
-    top_ips = collection.aggregate([
-        {'$group':
-            {
-                "_id": '$ip',
-                "count": {'$sum': 1}
-            }
-        },
-        {'$sort': {"count": -1}},
-        {'$limit': 10},
-    ])
+    top_ips = collection.aggregate([{'$group':
+                                     {
+                                         "_id": '$ip',
+                                         "count": {'$sum': 1}
+                                     }
+                                     },
+                                    {'$sort': {"count": -1}},
+                                    {'$limit': 10},
+                                    ])
     for ip in top_ips:
         print(f'\t{ip.get("_id")}: {ip.get("count")}')
