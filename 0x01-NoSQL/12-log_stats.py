072b22767c27621c806668eda3bd8c1639007c4b
elayneh <betenatefera@gmail.com>
2022-07-05 22:29:52 +0300

    task added

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env/ python3
 """  Python script that provides some stats about Nginx logs stored in MongoDB """
 
-import pymongo
+from tabnanny import check
+from pymongo import MongoClient
 
+if __name__ == "__main__":
+    """ check presence of elements """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client["logs"]
+    coll = db["nginx"]
+
+    print(f"{coll.estimated_document_count()} logs")
+    print("Methods:")
+    for method in ["GET", "PUT", "POST", "PATCH", "DELETE"]:
+        mc = coll.count_documents({"method": method})
+        print("\tmethod {}: {}". format(method, mc))
+    getter = coll.count_documents({
+        'method': 'GET', 'path': "/status"
+    })
+    print("{} status check". format(getter))
