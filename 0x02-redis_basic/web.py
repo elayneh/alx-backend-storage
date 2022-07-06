66c5c4072e2ecd85fd6f5779fdc615624a5c5656
elayneh <betenatefera@gmail.com>
2022-07-06 22:46:22 +0300

    modified

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -4,15 +4,14 @@
 import redis
 import requests
 r = redis.Redis()
-count = 0
 
 
 def get_page(url: str) -> str:
-    """ track how many times a particular URL was accessed in the key
-        "count:{url}"
-        and cache the result with an expiration time of 10 seconds """
+    count = 0
+    """ track how many times a
+    particular URL was accessed in the key count:{url} """
     r.set(f"cached:{url}", count)
-    resp = requests.get(ur)
+    resp = requests.get(url)
     r.incr(f"count:{url}")
     r.setex(f"cached:{url}", 10, r.get(f"cached:{url}"))
     return resp.text
